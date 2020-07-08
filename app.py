72877d24146117c9a3e2a4e817460b19d0984342
Sanketh Vidap <45508717+sankethv@users.noreply.github.com>
2020-07-09 02:56:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 01:57:34 2020
+
+@author: vsanketh
+"""
+
 from flask import Flask, render_template, request
-import jsonify
+from flask_jsonpify import jsonify
 import requests
 import pickle
 import numpy as np
@@ -20,15 +27,18 @@ def predict():
         Year = int(request.form['Year'])
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
+
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol=='Diesel'):          
+                Fuel_Type_Petrol=0
+                Fuel_Type_Diesel=1
         else:
             Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
+            Fuel_Type_Diesel=0
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
@@ -40,7 +50,7 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
@@ -51,4 +61,3 @@ def predict():
 
 if __name__=="__main__":
     app.run(debug=True)
-
